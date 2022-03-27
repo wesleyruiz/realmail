8b3962d262c1d8194fdb29ec33ede86b394d413c
maocanhua <962491243@qq.com>
2022-03-27 13:55:09 +0800

    Group

--- packages/realmail-extensions/src/AttributePanel/components/blocks/Group/index.tsx
+++ packages/realmail-extensions/src/AttributePanel/components/blocks/Group/index.tsx
@@ -2,39 +2,54 @@ import React from 'react';
 import { Width } from '@extensions/AttributePanel/components/attributes/Width';
 import { BackgroundColor } from '@extensions/AttributePanel/components/attributes/BackgroundColor';
 import { VerticalAlign } from '@extensions/AttributePanel/components/attributes/VerticalAlign';
-import { Collapse, Grid } from '@arco-design/web-react';
+import { Collapse, Grid, Space, Tabs } from '@arco-design/web-react';
 import { AttributesPanelWrapper } from '@extensions/AttributePanel/components/attributes/AttributesPanelWrapper';
 import { ClassName } from '../../attributes/ClassName';
 import { CollapseWrapper } from '../../attributes/CollapseWrapper';
+import { useFocusIdx, IconFont } from 'realmail-editor';
 
 export function Group() {
   return (
     <AttributesPanelWrapper>
-      <CollapseWrapper defaultActiveKey={['0', '1', '2']}>
-        <Collapse.Item name='0' header='Dimension'>
-          <Grid.Row>
-            <Grid.Col span={11}>
-              <Width />
-            </Grid.Col>
-            <Grid.Col offset={1} span={11}>
-              <VerticalAlign />
-            </Grid.Col>
-          </Grid.Row>
-        </Collapse.Item>
-        <Collapse.Item name='1' header='Background'>
-          <Grid.Row>
-            <Grid.Col span={11}>
-              <BackgroundColor />
-            </Grid.Col>
-            <Grid.Col offset={1} span={11} />
-          </Grid.Row>
-        </Collapse.Item>
-        <Collapse.Item name='4' header='Extra'>
-          <Grid.Col span={24}>
-            <ClassName />
-          </Grid.Col>
-        </Collapse.Item>
-      </CollapseWrapper>
+      <Tabs type='card-gutter'>
+        <Tabs.TabPane title={<Space><IconFont size={12} iconName='icon-desktop' /><span>Desktop</span></Space>} key="1">
+          <AttributesContainer mode="desktop" />
+        </Tabs.TabPane>
+        <Tabs.TabPane title={<Space><IconFont iconName='icon-mobile' /><span>Mobile</span></Space>} key="2">
+          <AttributesContainer mode='mobile' />
+        </Tabs.TabPane>
+      </Tabs>
     </AttributesPanelWrapper>
+  );
+}
+
+function AttributesContainer({ mode }: { mode: 'desktop' | 'mobile'; }) {
+  const { focusIdx } = useFocusIdx();
+  return (
+    <CollapseWrapper defaultActiveKey={['0', '1', '2']}>
+      <Collapse.Item name='0' header='Dimension'>
+        <Grid.Row>
+          <Grid.Col span={11}>
+            <Width name={mode === 'desktop' ? `${focusIdx}.attributes.Width` : `${focusIdx}.mobileAttributes.Width`} />
+          </Grid.Col>
+          <Grid.Col offset={1} span={11}>
+            <VerticalAlign name={mode === 'desktop' ? `${focusIdx}.attributes.vertical-align` : `${focusIdx}.mobileAttributes.vertical-align`} />
+          </Grid.Col>
+        </Grid.Row>
+      </Collapse.Item>
+      <Collapse.Item name='1' header='Background'>
+        <Grid.Row>
+          <Grid.Col span={11}>
+            <BackgroundColor name={mode === 'desktop' ? `${focusIdx}.attributes.background-color` : `${focusIdx}.mobileAttributes.background-color`} />
+          </Grid.Col>
+          <Grid.Col offset={1} span={11} />
+        </Grid.Row>
+      </Collapse.Item>
+      <Collapse.Item name='4' header='Extra'>
+        <Grid.Col span={24}>
+          <ClassName name={mode === 'desktop' ? `${focusIdx}.attributes.css-class` : `${focusIdx}.mobileAttributes.css-class`} />
+        </Grid.Col>
+      </Collapse.Item>
+    </CollapseWrapper>
   );
 }
